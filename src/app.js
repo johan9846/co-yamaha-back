8ad89adbd86cf99f938427aa728602c984b4fedc
johan9846 <johan@ayte.co>
2025-03-10 11:37:53 -0500

    [FEATURE] TEST2

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Registrar rutas de tablas
+// Registrar rutas 
 app.use("/categories", categoryRoutes);
 app.use("/products", productRoutes);
 
